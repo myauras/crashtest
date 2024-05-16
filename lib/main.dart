90f9c616268e318d9bf5cee18b4abe1a63847ce3
scozirge <scozirge2@gmail.com>
2024-05-16 16:19:37 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'menu_screen.dart';
+import 'screens/simple_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Unity Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MenuScreen(),
+        '/simple': (context) => const SimpleScreen(),
+      },
+    );
+  }
+}
